b777e04baf08d7f07e80413903f8b58f45172b42
Anthony Eli Agbenu <lefootballroi@yahoo.com>
2014-04-26 02:42:42 +0000

    editted wsgi.py file

--- cybermall/settings.py
+++ cybermall/settings.py
@@ -103,3 +103,10 @@ TEMPLATE_DIRS=(
     'templates/computers/',
     'templates/home/',
 )
+
+
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
